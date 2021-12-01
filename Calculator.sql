acc5da01f340a2bc268108ae37e378d58d0f7cd9
AdinaQ <61895538+AdinaQ@users.noreply.github.com>
2021-12-01 02:43:57 -0500

    Update Calculator.sql

--- Calculator.sql
+++ Calculator.sql
@@ -21,8 +21,8 @@ SELECT c.operand1,c.operator,c.operand2,case
     when c.operator='+' then a.value+b.value
     when c.operator='-' then a.value-b.value
     when c.operator='*' then a.value*b.value
-    when c.operator='/' and b.value=0 then 'Error: division by 0'
-    when c.operator='/' and b.value!=0 then round(a.value/b.value,2)
-    end as result
-from calculations c left join variables a on c.operand1=a.variable 
-left join variables b on c.operand2=b.variable;
+    when c.operator='/' AND b.value=0 then 'Error: division by 0'
+    when c.operator='/' AND b.value!=0 then round(a.value/b.value,2)
+    end AS result
+FROM calculations c LEFT JOIN variables a ON c.operand1=a.variable 
+LEFT JOIN variables b ON c.operand2=b.variable;
